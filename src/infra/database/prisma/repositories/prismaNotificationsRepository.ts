ca8da59e8ba731dcfc6a8c7c26bb9a150c7d8930
zorasantos <zoranildo22@gmail.com>
2023-01-05 23:58:26 -0300

    feat: implements methods in prisma repository

--- src/infra/database/prisma/repositories/prismaNotificationsRepository.ts
+++ src/infra/database/prisma/repositories/prismaNotificationsRepository.ts
@@ -7,8 +7,46 @@ import { PrismaNotificationMapper } from '../mappers/prismaNotificationMapper';
 @Injectable()
 export class PrismaNotificationsRepository implements ANotificationRepository {
   constructor(private prismaService: PrismaService) {}
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = await this.prismaService.notification.findUnique({
+      where: {
+        id: notificationId,
+      },
+    });
+
+    if (!notification) {
+      return null;
+    }
+
+    return PrismaNotificationMapper.toDomain(notification);
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    const count = await this.prismaService.notification.count({
+      where: { recipientId },
+    });
+
+    return count;
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    const notifications = await this.prismaService.notification.findMany({
+      where: {
+        id: recipientId,
+      },
+    });
+
+    return notifications.map(PrismaNotificationMapper.toDomain);
+  }
+
   async save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prismaService.notification.update({
+      where: { id: raw.id },
+      data: raw,
+    });
   }
 
   async create(notification: Notification): Promise<void> {
@@ -17,9 +55,5 @@ export class PrismaNotificationsRepository implements ANotificationRepository {
     await this.prismaService.notification.create({
       data: raw,
     });
-  }
-
-  async findById(notificationId: string): Promise<Notification | null> {
-    throw new Error('Method not implemented.');
   }
 }
